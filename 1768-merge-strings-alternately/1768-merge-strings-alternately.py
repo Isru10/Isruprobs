62db0f6cc8b3b3cf1a21a4ff8a088b9641a48d2e
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-05 23:54:26 -0800

    Time: 38 ms (63.24%), Space: 17.4 MB (11.46%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
         i=0
+        j=0
         l=[]
-        while i<len (word1) or i<len(word2):
-                if i<len(word1):
-                    l.append(word1[i])
-                if i<len(word2):
-                    l.append(word2[i])
-                i+=1
-                
-        return ''.join(l )
-              
-
-      
-
-        
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        
+        while i < len(word1) or j<len (word2):
+            if  i < len(word1): 
+                l.append(word1[i])
+            if  j < len(word2): 
+                l.append(word2[j])
+            i+=1
+            j+=1
+        return ''.join(l)   
