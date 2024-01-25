8ef2f377a853ca4156c2ebe28868ff9465b0b0fb
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-25 10:08:58 -0800

    Time: 42 ms (91.22%), Space: 16.9 MB (63.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,7 @@
 from collections import *
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        ds={}
-        dt={}
-        for c in s:
-            ds[c]=ds.get(c, 0)+1
-        for c in t:
-            dt[c]=dt.get(c, 0)+1
+        return Counter (s)==Counter (t)
         
-        if ds==dt:
-            return True
-        else:
-            return False
         
         
