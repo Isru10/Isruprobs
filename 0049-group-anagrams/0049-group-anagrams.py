7ef275e562b619f2a72f0fd1da8ed7c53895cc10
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-26 11:46:46 -0800

    Time: 97 ms (49.08%), Space: 20 MB (82.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,8 @@
 from collections import Counter 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if strs=='':
-            return [['']]
-        d=defaultdict(list)
-        for i in strs:
-            d[hash(tuple(sorted(Counter(i).items())))].append(i)
-        return d.values()
-        
+        ans=defaultdict(list)
+        for word in strs :
+            sortedword=' '.join(sorted(word))
+            ans[sortedword].append(word)
+        return ans.values()
