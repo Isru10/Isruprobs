6dfa06634edfff6ecbc6254a2cc6b2c0cd2f0acf
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-29 08:42:08 -0800

    Time: 85 ms (88.98%), Space: 17.3 MB (72.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t=="": return ""
+        if s=="": return ""
         window,countT={},{}
-        for  c in t:
-            countT[c]=countT.get(c,0)+1
-        have , need =0, len(countT)
-        res, resLen=[-1,-1],float("inf")
-        l=0
-        for r  in range(len(s)):
-            c=s[r]
-            window[c]=1+window.get(c,0)
+        for i in t:
+            countT[i]=countT.get(i, 0 )+1
             
-            if c in countT and window[c]==countT[c]:
+        have , need =0, len(countT)
+        reslen,res=float("inf"),[-1,-1]
+        l=0
+        
+        for r in range(len(s)):
+            c=s[r]
+            window[c]=window.get(c,0)+1
+            if c in t and countT[c]==window[c]:
                 have+=1
             while have == need :
-                if (r-l+1)<resLen:
+                if r-l+1 <reslen:
                     res=[l,r]
-                    resLen=(r-l+1)
-                
+                    reslen=min(reslen,r-l+1)
                 window[s[l]]-=1
-                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                if s[l]in countT and window[s[l]] <countT[s[l]] :
                     have-=1
                 l+=1
         l,r=res
-        return s[l:r+1] if resLen!=float("inf") else ""
-                
+        return s[l:r+1]
+            
                     
                     
-                    
-                    
-                    
-                    
+            
+            
+            
+            
+            
+            
