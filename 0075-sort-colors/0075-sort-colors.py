21bab43f541337af346765c894f482615f160e80
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-30 03:21:37 -0800

    Time: 39 ms (60.01%), Space: 16.5 MB (71.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,12 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k= len(nums)
+        k=len(nums)
         for i in range(k):
-            mini= i 
+            mini=i
             for j in range(i,k):
                 if nums[mini]>nums[j]:
                     mini=j
             nums[mini],nums[i]=nums[i], nums[mini]
-                    
-        return nums 
+        return nums
