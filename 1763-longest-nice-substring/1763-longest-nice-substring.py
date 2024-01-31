261a03c94e4757f7bf9892f81498afc04da6f568
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-31 12:13:04 -0800

    Time: 36 ms (89.59%), Space: 16.6 MB (54.07%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,14 +1,12 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
         if len(s)<2:
-            return ''
+            return ""
         seen=set(s)
-        for i in range (len(s)):
-            if   s[i].lower() in seen and s[i].upper() in seen:
+        for i in range( len (s)):
+            if s[i ].lower() in seen and s[i].upper() in seen :
                 continue 
-            left=self.longestNiceSubstring(s[:i])
-            right=self.longestNiceSubstring(s[i+1:])
-            return max(left, right , key=len)
-        return s 
-            
-        
+            l=self.longestNiceSubstring( s[:i])
+            r=self.longestNiceSubstring(s[i+1:])
+            return max(l, r, key=len)
+        return s
