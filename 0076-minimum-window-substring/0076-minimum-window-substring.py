d86431d5a40d3ced36622df3bdde8240cb9aa7a2
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-01-31 22:06:38 -0800

    Time: 79 ms (94.37%), Space: 17.3 MB (63.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if s=="": return ""
-        countT, window = {},{}
-        
+        window,tcount={},{}
         for i in t:
-            countT[i]=countT.get(i,0)+1
-        reslen, res=float("inf"), [-1,-1]
-        need, have =len(countT),0
+            tcount[i]=tcount.get(i,0)+1
+        need,have =len(tcount), 0
+        reslen,res=float("inf"),[-1,-1]
         l=0
-        for r in range(len(s)):
+        for r in range(len (s)):
             c=s[r]
-            window[c]=window.get(c,0)+1
-            if c in countT and window[c]==countT[c]:
+            window[c]=window.get (c, 0)+1
+            if c in tcount and window[c]==tcount[c]:
                 have+=1
-            while need==have:
-                if (r-l+1) < reslen:
+            while have==need:
+                if (r-l+1 )< reslen:
+                    reslen=r-l+1
                     res=[l,r]
-                    reslen=min(reslen, r-l+1)
                 window[s[l]]-=1
-                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                if s[l] in tcount and window[s[l]]<tcount[s[l]]:
                     have-=1
                 l+=1
         l,r=res
         return s[l:r+1]
-            
-                    
-                    
-            
-            
-            
-            
-            
-            
