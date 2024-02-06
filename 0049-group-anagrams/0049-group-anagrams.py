3dee71ce532ad6992c38e49361fd763f472c9ce3
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-02-06 05:22:22 -0800

    Time: 92 ms (57.61%), Space: 20.1 MB (78.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans=defaultdict(list)
-        for word in strs :
-            sortedword=' '.join(sorted(word))
+        for word in strs:
+            sortedword=''.join(sorted(word))
             ans[sortedword].append(word)
         return ans.values()
