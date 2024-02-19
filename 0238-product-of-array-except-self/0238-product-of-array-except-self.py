d74b35247f75eb44ea83badb409bd28cb604a6bd
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-02-18 23:54:47 -0800

    Time: 165 ms (67.92%), Space: 23.9 MB (75.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,8 +3,8 @@ class Solution:
         ans=[1]*len(nums)
         for i in range(1,len(nums)):
             ans[i]=ans[i-1]*nums[i-1]
-        rightsum=1
-        for i in range(len(nums)-1, -1,-1):
-            ans[i]*=rightsum
-            rightsum*=nums[i]
+        rightp=1
+        for i in range(len(nums)-1,-1,-1):
+            ans[i]=ans[i]*rightp
+            rightp=rightp*nums[i]
         return ans 
