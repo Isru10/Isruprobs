f1f563fd674200dba9a492d17915b315dbe1ffcc
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-02-18 23:27:22 -0800

    Time: 27 ms (96.66%), Space: 16.6 MB (53.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,9 +1,5 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n<=0:
-            return False
-        if math.log2(n).is_integer():
-            return True
-        else:
-            return False
-        
+        if n <=0:return False
+        if( log2(n)).is_integer():return True
+        else:return False
