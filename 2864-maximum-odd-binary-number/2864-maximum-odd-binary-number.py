6905b1a7ff940ada10017bceb95d1a57f811e95a
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-02-29 23:48:28 -0800

    Time: 45 ms (18.53%), Space: 16.6 MB (75.75%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        n=len(s)
+        arr=["0"]*n
+        r=n-1
+        l=0
+        for i in range(n):
+            if s[i]=="1" and "1" not in arr:
+                arr[r]=s[i]
+                i+=1
+                continue 
+            elif s[i]=="1" and "1" in arr:
+                arr[l]=s[i]
+                l+=1
+                i+=1
+                continue
+               
+        return "".join(arr)
+            
