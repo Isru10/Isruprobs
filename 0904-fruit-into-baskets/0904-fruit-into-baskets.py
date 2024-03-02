864675343bc309665f3f9973aafb39477634067b
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-03-02 08:05:53 -0800

    Time: 691 ms (44.67%), Space: 24.1 MB (65.59%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,16 +1,15 @@
 class Solution:
     def totalFruit(self, nums: List[int]) -> int:
-        l=0
         counter=defaultdict(int)
         res=0
+        l=0
         for r in range(len(nums)):
             counter[nums[r]]+=1
-          
-            while l<r and  len(counter) >2 :
+            while len(counter)>2:
                 counter[nums[l]]-=1
-      
                 if counter[nums[l]]<1:
-                     counter.pop(nums[l])
+                    counter.pop(nums[l])
                 l+=1
             res=max(res,r-l+1)
-        return res
+        return res 
+            
