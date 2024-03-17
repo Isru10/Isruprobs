e556e719d2d6448e6e8d779351d38517f0aa0dd1
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-03-17 03:20:28 -0800

    Time: 66 ms (94.56%), Space: 19.9 MB (79.86%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,14 +1,11 @@
 class Solution:
     def insert(self, i: List[List[int]], ni: List[int]) -> List[List[int]]:
-        
         i.append(ni)
-        i.sort(key=lambda x: x[0])
+        i.sort(key=lambda x:x[0])
         ns=[i[0]]
         for it in i[1:]:
-            if ns[-1][1]<it[0]:
+            if it[0]>ns[-1][1]:
                 ns.append(it)
             else:
-                ns[-1][1]=max(ns[-1][1], it[1])
+                ns[-1][1]=max(ns[-1][1],it[1])
         return ns
-                
-     
