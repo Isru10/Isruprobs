9e3abe44b701ae9114a8e96b249fe42d76ad8a38
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-03-27 14:49:38 +0200

    Time: 505 ms (56.71%), Space: 19.3 MB (50.02%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -5,12 +5,11 @@ class Solution:
         cnt=0
         prd=1
         l=0
-        r=0
-        while r<len(nums) :
+        
+        for r in range(len(nums)):
             prd*=nums[r]
             while prd>=k:
-                prd//=nums[l]
+                prd=prd//nums[l]
                 l+=1
-            cnt+=1+(r-l)
-            r+=1
-        return cnt 
+            cnt+=(r-l+1)
+        return cnt
