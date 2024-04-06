5ce949bc7f3fa377e6e43571d8e81e7c32804ec4
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-04-06 14:33:35 +0200

    Time: 56 ms (33.21%), Space: 16.8 MB (88.43%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,10 +2,10 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack=[]
         for i in nums1:
-            for j in range(nums2.index(i)+1, len(nums2)):
+            for j in range(nums2.index(i)+1,len(nums2)):
                 if nums2[j]>i:
                     stack.append(nums2[j])
-                    break
+                    break 
             else:
                 stack.append(-1)
         return stack 
