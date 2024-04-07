d2ded81802a906d21556c4acb65621da1af395da
Isru10 <149589109+Isru10@users.noreply.github.com>
2024-04-07 13:03:45 +0200

    Time: 26 ms (95.95%), Space: 16.6 MB (37.44%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -5,8 +5,9 @@ class Solution:
         for i , c in enumerate(s):
             if c=="(":
                 mul*=2
-            elif c ==")":
+            elif c==")":
                 if s[i-1]=="(":
                     tot+=mul//2
-                mul= mul//2
+                mul=mul//2
         return tot 
+        
